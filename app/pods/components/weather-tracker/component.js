8014ca8652925f106c31efef570c480e6306efa5
Steve Jabour <steve@jabour.me>
2017-03-27 16:02:28 -0700

    Simple loading weather state and action

--- app/pods/components/weather-tracker/component.js
+++ app/pods/components/weather-tracker/component.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isLoading: false,
 
+  actions: {
+    loadWeather(zip) {
+      if (zip.length === 5) {
+        this.set('isLoading', true);
+
+        Ember.$.get('/weather')
+          .then(weather => this.set('weather', weather))
+          .then(() => this.set('isLoading', false));
+      }
+    }
+  }
 });
