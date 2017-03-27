f7a2b9754c83e8f68f392e43e559988311c21a67
Steve Jabour <steve@jabour.me>
2017-03-27 15:40:17 -0700

    Example solution with hooks

--- app/pods/3-side-effects/controller.js
+++ app/pods/3-side-effects/controller.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    aSaveActionHook(user) {
+      console.info('We saved!', user);
+    },
 
+    aSaveErrorActionHook(user) {
+      console.error('We tried to save, but we failed!', user);
+    }
+  }
 });
--- app/pods/components/edit-user-form/component.js
+++ app/pods/components/edit-user-form/component.js
@@ -3,12 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   user: null,
 
+  'did-save': function() {
+    console.info('Save was clicked!', ...arguments);
+  },
+
+  'did-error': function() {
+    console.error('Save was clicked, but oops!', ...arguments);
+  },
+
   actions: {
     save() {
       return this.get('user').save()
-        .then(() => {
-          console.log('Save was clicked!');
-        });
+        .then(this.get('did-save'))
+        .catch(this.get('did-error'));
     }
   }
 });
