3fd1101dcd834a2f7520b4bdc1f97e68a3da1222
Steve Jabour <steve@jabour.me>
2017-03-27 15:24:37 -0700

    This is the domain-specific soultion
    The edit form does not allow you to override it's save action.
    It just does the job for you.

--- app/pods/3-side-effects/controller.js
+++ app/pods/3-side-effects/controller.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    save(user) {
-      return user.save()
-        .then(() => {
-          console.log('The user was saved by the controller');
-        });
-    }
-  }
+
 });
--- app/pods/components/edit-user-form/component.js
+++ app/pods/components/edit-user-form/component.js
@@ -3,13 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   user: null,
 
-  'on-save-click': function(user) {
-    console.log('Save was clicked!', user);
-  },
-
   actions: {
     save() {
-      this.get('on-save-click')(this.get('user'));
+      return this.get('user').save()
+        .then(() => {
+          console.log('Save was clicked!');
+        });
     }
   }
 });
