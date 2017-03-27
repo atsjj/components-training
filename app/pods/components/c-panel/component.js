d62a8bf80215b714f9b03617b28b1066aff640b9
Steve Jabour <steve@jabour.me>
2017-03-27 14:06:04 -0700

    Polished solution

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
 
-const get = Ember.get;
-const set = Ember.set;
-
 export default Ember.Component.extend({
   isExpanded: true,
 
   actions: {
     accordian() {
-      const isExpanded = get(this, 'isExpanded');
-
-      set(this, 'isExpanded', !isExpanded);
+      this.toggleProperty('isExpanded');
     }
   }
 });
