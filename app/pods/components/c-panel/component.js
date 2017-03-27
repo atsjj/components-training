3285583f1de64a11aef6aed88fd57bfb0c5efc3d
Steve Jabour <steve@jabour.me>
2017-03-27 14:30:16 -0700

    I call this a janky way to solve the problem
    I would really like to see the action delegate so it can be overriden

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   title: 'Title here',
+  isFrozen: false,
   isExpanded: true,
 
   actions: {
     accordian() {
+      if (this.get('isFrozen')) {
+        return;
+      }
+
       this.toggleProperty('isExpanded');
     }
   }
