de11a1ca02cc1b7ad4b39ce1da4c4a21262c4f42
Steve Jabour <steve@jabour.me>
2017-03-27 13:49:56 -0700

    My solution to the problem of collapsing panels

--- app/pods/components/c-panel/component.js
+++ app/pods/components/c-panel/component.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
+const get = Ember.get;
+const set = Ember.set;
+
 export default Ember.Component.extend({
+  isExpanded: true,
+
+  actions: {
+    accordian() {
+      const isExpanded = get(this, 'isExpanded');
+
+      set(this, 'isExpanded', !isExpanded);
+    }
+  }
 });
